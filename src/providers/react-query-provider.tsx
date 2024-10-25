8bc5acf8623fa28cfe4d559eb809d37b8778e661
Richard Neves <ricknevesbc@icloud.com>
2024-10-25 18:39:52 -0300

    chore(react-query): add react-query devtools
    Adds react-query devtools for easier debugging and monitoring of queries.
    
    The devtools are currently disabled in production and can be enabled for development purposes.

--- src/providers/react-query-provider.tsx
+++ src/providers/react-query-provider.tsx
@@ -3,9 +3,17 @@
 import { PropsWithChildren, useState } from 'react'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export function ReactQueryProvider({ children }: PropsWithChildren) {
   const [client] = useState(new QueryClient())
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={client}>
+      {/* TODO: Remove this in production */}
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      {children}
+    </QueryClientProvider>
+  )
 }
