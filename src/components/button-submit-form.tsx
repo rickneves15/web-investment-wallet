c9083f5c782abee42cecf1097db0cbe21157b499
Richard Neves <ricknevesbc@icloud.com>
2024-10-26 12:44:48 -0300

    feat(ButtonSubmitForm): Adds className prop
    Adds a `className` prop to the `ButtonSubmitForm` component to allow for custom styling.

--- src/components/button-submit-form.tsx
+++ src/components/button-submit-form.tsx
@@ -3,21 +3,24 @@ import { PropsWithChildren } from 'react'
 import { Loader2 } from 'lucide-react'
 
 import { Button } from './ui/button'
+import { cn } from '~/lib/utils'
 
 type ButtonSubmitFormProps = PropsWithChildren & {
   disabled?: boolean
   isSubmitting?: boolean
+  className?: string
 }
 
 export function ButtonSubmitForm({
   children,
   disabled = false,
   isSubmitting = false,
+  className,
 }: ButtonSubmitFormProps) {
   return (
     <Button
       type="submit"
-      className="flex justify-between rounded-xl border border-blue-400 bg-blue-400 text-xs font-semibold leading-tight text-white hover:bg-blue-400/90"
+      className={cn("flex justify-between rounded-xl border border-blue-400 bg-blue-400 text-xs font-semibold leading-tight text-white hover:bg-blue-400/90", cla)}
       disabled={disabled}
     >
       {isSubmitting ? (
